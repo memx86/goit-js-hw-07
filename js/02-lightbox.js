0ed00b3d01a66cd4483e8b8baa714a6a89959fc9
Yevhen Malyshko <memx86@gmail.com>
2022-01-05 14:23:32 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightboxOptions = {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+};
+let gallery = new SimpleLightbox(".gallery a", lightboxOptions);
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+}
